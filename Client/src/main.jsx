ef889a2ca8648218b212e6ffa7688f638c9de041
EdvinNordin <nordin.edvin@gmail.com>
2025-01-30 01:00:24 +0100

    classic

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import Routing from './Routing'
 import './index.css'
 
-export const backendURL = 'http://localhost:3000/api/'
-//export const backendURL = 'https://tabs-share-backend.onrender.com/api/'
+//export const backendURL = 'http://localhost:3000/api/'
+export const backendURL = 'https://tabs-share-backend.onrender.com/api/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
